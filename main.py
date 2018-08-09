8a6d9a71eb38a917c570cd08c05f52ed65ffa6c0
RachaelSoh <rachael.suhendra012@gmail.com>
2018-08-09 10:18:49 +0700

    test

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def layout():
+def hello_world():
   return "Welcome to CBN Cloud Workshop!"
   
 if __name__ == '__main__':
-  app.run(debug=True)
+  app.run()
 
 
 
